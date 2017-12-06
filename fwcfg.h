6bb7096c3b29a4fc36f5353c46d4a93292fc5a61
Sven Eckelmann <sven.eckelmann@openmesh.com>
2017-12-06 17:26:44 +0100

    ap51-flash: Fix copyright of fwcfg files
    These files were open-sourced by OpenMesh and integrated in this GPLv3
    project. They are now licensed under the same terms as the rest of the
    project.

--- fwcfg.h
+++ fwcfg.h
@@ -1,12 +1,20 @@
-/***
+/*
+ * Copyright (C) Sven Eckelmann <sven.eckelmann@openmesh.com>
  *
- * Copyright (C) 2012-2015 Open Mesh, Inc.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public
+ * License as published by the Free Software Foundation.
  *
- * The reproduction and distribution of this software without the written
- * consent of Open-Mesh, Inc. is prohibited by the United States Copyright
- * Act and international treaties.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- ***/
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
 
 #ifndef _FWCFG_H
 #define _FWCFG_H
