7a5f6233158869fd26fe47978d2dcfedd182ade3
Sven Eckelmann <sven@narfation.org>
2017-12-09 09:10:23 +0100

    ap51-flash: Sort the router types in router_tftp_client.h
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- router_tftp_client.h
+++ router_tftp_client.h
@@ -24,21 +24,21 @@
 
 struct node;
 
-extern const struct router_type mr500;
-extern const struct router_type mr600;
-extern const struct router_type mr900;
-extern const struct router_type mr1750;
-extern const struct router_type a40;
-extern const struct router_type a42;
-extern const struct router_type a60;
-extern const struct router_type a62;
-extern const struct router_type om2p;
-extern const struct router_type om5p;
-extern const struct router_type om5pan;
-extern const struct router_type om5pac;
-extern const struct router_type p60;
-extern const struct router_type d200;
-extern const struct router_type g200;
+extern const struct router_type  a40;
+extern const struct router_type  a42;
+extern const struct router_type  a60;
+extern const struct router_type  a62;
+extern const struct router_type  d200;
+extern const struct router_type  g200;
+extern const struct router_type  mr1750;
+extern const struct router_type  mr500;
+extern const struct router_type  mr600;
+extern const struct router_type  mr900;
+extern const struct router_type  om2p;
+extern const struct router_type  om5p;
+extern const struct router_type  om5pac;
+extern const struct router_type  om5pan;
+extern const struct router_type  p60;
 
 void tftp_client_flash_time_set(struct node *node);
 int tftp_client_flash_completed(struct node *node);
