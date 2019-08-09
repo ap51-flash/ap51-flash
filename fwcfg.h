fac11f9a3ccf71afb82c0af58f443693c2f588ee
Sven Eckelmann <sven@narfation.org>
2019-08-09 12:12:04 +0200

    ap51-flash: Reduce copyright boilerplate
    The SPDX license information and the shipped LICENSE files are enough to
    correctly identify under which license each file is.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- fwcfg.h
+++ fwcfg.h
@@ -1,21 +1,5 @@
-/*
+/* SPDX-License-Identifier: GPL-3.0-or-later
  * SPDX-FileCopyrightText: Sven Eckelmann <sven.eckelmann@openmesh.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 3 of the GNU General Public
- * License as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #ifndef __AP51_FLASH_FWCFG_H__
--- socket.c
+++ socket.c
@@ -1,21 +1,5 @@
-/*
- * SPDX-FileCopyrightText: Marek Lindner <mareklindner@neomailbox.ch>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 3 of the GNU General Public
- * License as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
+/* SPDX-FileCopyrightText: Marek Lindner <mareklindner@neomailbox.ch>
  */
 
 #include "socket.h"
