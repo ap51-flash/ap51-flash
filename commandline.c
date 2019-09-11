ap51-flash/ap51-flash

commit 363933b9f2b47a9604835fd5fefa6591c80a68bd

ap51-flash: Add MAC address whitelist option
Adds -m as a command-line option to whitelist MAC addresses for flashing.
You can use -m multiple times to whitelist several MAC addresses.
For example:

  $ ./ap51-flash -m aa:bb:cc:dd:ee:ff -m 12:34:56:78:90:ab 1 image.bin

Signed-off-by: Ian Ling <ian@iancaling.com>

commandline.c
@@ -11,6 +11,7 @@
 
 #include "flash.h"
 #include "router_images.h"
+#include "router_types.h"
 #include "socket.h"
 
 #ifndef SOURCE_VERSION
@@ -20,10 +21,11 @@
 static void usage(const char *prgname)
 {
 	fprintf(stderr, "Usage:\n");
-
-	fprintf(stderr, "%s interface image\tflash router with given image\n",
+	fprintf(stderr, "%s interface image\t\t\tflash router with given image\n",
 		prgname);
-	fprintf(stderr, "%s -v\t\t\tprints version information\n", prgname);
+	fprintf(stderr, "%s [-m <MAC>...] interface image\tflash router at given MAC address(es)\n", prgname);
+	fprintf(stderr, "%s -h\t\t\t\t\tshow usage/help\n", prgname);
+	fprintf(stderr, "%s -v\t\t\t\t\tprints version information\n", prgname);
 
 	fprintf(stderr, "\nOne or multiple images of the following type can be specified:\n");
 	router_images_print_desc();
@@ -42,12 +44,13 @@ int main(int argc, char* argv[])
 	bool load_embedded = true;
 	const char *progname = "ap51-flash";
 	static struct option long_options[] = {
-		{"help",	no_argument,	0,	'h'},
-		{"version",	no_argument,	0,	'v'},
+		{"help",	no_argument,		0,	'h'},
+		{"version",	no_argument,		0,	'v'},
+		{"mac",		required_argument,	0,	'm'},
 		{}
 	};
 
-	while ((c = getopt_long(argc, argv, "hv", long_options, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "hvm:", long_options, NULL)) != -1) {
 		switch (c) {
 		case 'h':
 			print_help = true;
@@ -57,6 +60,10 @@ int main(int argc, char* argv[])
 			print_version = true;
 			ret = 0;
 			break;
+		case 'm':
+			if (mac_whitelist_add(optarg) == 0)
+				break;
+			/* fall-through */
 		case '?':
 			print_help = true;
 			break;
@@ -76,8 +83,11 @@ int main(int argc, char* argv[])
 #else
 		printf("ap51-flash (%s)\n", SOURCE_VERSION);
 #endif
-		return 0;
+		goto out;
 	}
+
+	argc -= optind;
+	argv += optind;
 
 	if (argc < 2) {
 		fprintf(stderr, "Error - no interface specified\n");
@@ -85,14 +95,14 @@ int main(int argc, char* argv[])
 		goto out;
 	}
 
-	if (strlen(argv[1]) < 3)
-		iface = socket_find_iface_by_index(argv[1]);
+	if (strlen(argv[0]) < 3)
+		iface = socket_find_iface_by_index(argv[0]);
 
 	if (!iface)
-		iface = argv[1];
+		iface = argv[0];
 
-	argc -= 2;
-	argv += 2;
+	argc -= 1;
+	argv += 1;
 
 	router_images_init();
 

Makefile
@@ -75,7 +75,7 @@ ifeq ($(PLATFORM),LINUX)
   BINARY_SUFFIX =
 else ifeq ($(PLATFORM),WIN32)
   BINARY_SUFFIX = .exe
-  CPPFLAGS += -D_CONSOLE -D_MBCS
+  CPPFLAGS += -D_CONSOLE -D_MBCS -D__USE_MINGW_ANSI_STDIO=1
 
   ifeq ($(origin PKG_CONFIG), undefined)
     PKG_CONFIG = $(CROSS)pkg-config

router_types.c
@@ -4,9 +4,13 @@
 
 #include "router_types.h"
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "compat.h"
 #include "flash.h"
+#include "list.h"
 #include "router_images.h"
 #include "router_redboot.h"
 #include "router_tftp_client.h"
@@ -20,6 +24,7 @@
 #endif
 
 int router_types_priv_size = 0;
+static struct mac_whitelist_entry *mac_whitelist_head;
 
 static const struct router_type *router_types[] = {
 	&a40,
@@ -43,6 +48,54 @@ static const struct router_type *router_types[] = {
 	&ap121f,
 	NULL,
 };
+
+static int read_mac(uint8_t mac[ETH_ALEN], const char *macstr)
+{
+	int ret;
+
+	ret = sscanf(macstr, "%02hhX:%02hhX:%02hhX:%02hhX:%02hhX:%02hhX",
+		     &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+	if (ret != 6)
+		ret = sscanf(macstr, "%02hhX-%02hhX-%02hhX-%02hhX-%02hhX-%02hhX",
+			     &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+
+	return (ret == 6);
+}
+
+int mac_whitelist_add(const char *macstr)
+{
+	uint8_t mac[ETH_ALEN];
+	struct mac_whitelist_entry *new_entry;
+
+	if (!read_mac(mac, macstr)) {
+		fprintf(stderr, "Error - could not add MAC address to whitelist: %s\n", macstr);
+		return -EINVAL;
+	}
+
+	new_entry = malloc(sizeof(*new_entry));
+	if (!new_entry) {
+		fprintf(stderr, "Error - could not allocate memory for new MAC whitelist entry\n");
+		return -ENOMEM;
+	}
+
+	memcpy(new_entry->mac, mac, ETH_ALEN);
+	new_entry->next = mac_whitelist_head;
+	mac_whitelist_head = new_entry;
+
+	return 0;
+}
+
+static bool mac_whitelist_find(const uint8_t *mac)
+{
+	struct mac_whitelist_entry *current;
+
+	slist_for_each (current, mac_whitelist_head) {
+		if (memcmp(mac, current->mac, ETH_ALEN) == 0)
+			return true;
+	}
+
+	return false;
+}
 
 int router_types_init(void)
 {
@@ -98,6 +151,19 @@ int router_types_detect_main(struct node *node, const char *packet_buff,
 		/* we detected a router that we have no image for */
 		if ((*router_type)->image->file_size < 1) {
 			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: is of type '%s' that we have no image for\n",
+				node->his_mac_addr[0], node->his_mac_addr[1],
+				node->his_mac_addr[2], node->his_mac_addr[3],
+				node->his_mac_addr[4], node->his_mac_addr[5],
+				(*router_type)->desc);
+
+			node->status = NODE_STATUS_NO_FLASH;
+			ret = 0;
+			break;
+		}
+
+		/* we detected a router whose MAC is not whitelisted */
+		if (mac_whitelist_head != NULL && !mac_whitelist_find(node->his_mac_addr)) {
+			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: is of type '%s' but MAC does not match MAC filter\n",
 				node->his_mac_addr[0], node->his_mac_addr[1],
 				node->his_mac_addr[2], node->his_mac_addr[3],
 				node->his_mac_addr[4], node->his_mac_addr[5],

router_types.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include "ap51-flash.h"
+#include "compat.h"
 
 struct node;
 
@@ -34,10 +35,16 @@ struct router_type {
 	int priv_size;
 };
 
+struct mac_whitelist_entry {
+	uint8_t mac[ETH_ALEN];
+	struct mac_whitelist_entry *next;
+};
+
 int router_types_init(void);
 void router_types_detect_pre(const uint8_t *our_mac);
 int router_types_detect_main(struct node *node, const char *packet_buff,
 			     int packet_buff_len);
+int mac_whitelist_add(const char *macstr);
 
 extern int router_types_priv_size;
 

commit 24db6a25b256160c9c5ce915e5417b86a01bf5ac

ap51-flash: allow starting ap51-flash without specifying image name
Fixes: 363933b9f2b47a9604835fd5fefa6591c80a68bd

Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

commandline.c
@@ -89,7 +89,7 @@ int main(int argc, char* argv[])
 	argc -= optind;
 	argv += optind;
 
-	if (argc < 2) {
+	if (argc < 1) {
 		fprintf(stderr, "Error - no interface specified\n");
 		usage(progname);
 		goto out;