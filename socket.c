ap51-flash/ap51-flash

commit e9f2088bd9dc314be3176d2bf22377745c48f167

ap51-flash: Retry RTNL reply recv after interruption
The recv call on the AF_NETLINK socket can be interrupted by a signal.
Instead of stopping the receive process completely, the receiving can be
retried immediatelly again.

Signed-off-by: Sven Eckelmann <seckelmann@datto.com>

socket.c
@@ -80,9 +80,12 @@ peek_retry:
 	if (!*nh)
 		goto close_sock;
 
+recv_retry:
 	rlen = recv(sock, *nh, rlen, 0);
-
 	if (rlen < 0) {
+		if (errno == EINTR)
+			goto recv_retry;
+
 		fprintf(stderr,
 			"Error - unable to receive netlink request: %s\n",
 			strerror(errno));

commit 53f08f111aee3e5f8c00326d705966a59b9f3e12

ap51-flash: Fix retry of RTNL message download
The expected message size is also the size of our allocated buffer. This
size is first retrieved via the peek into the buffer. And it is then given
as size argument to the actual recv call. And this actual recv call can be
called multiple times in case the system call was interrupted.

The actual receive call must therefore not overwrite the size information
or otherwise it will fail to download the correct data on the second try.

Fixes: e9f2088bd9dc ("ap51-flash: Retry RTNL reply recv after interruption")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

socket.c
@@ -34,6 +34,7 @@ static int socket_rtnl_recvmsg(int sock, struct nlmsghdr **nh,
 			       unsigned int *len)
 {
 	ssize_t rlen;
+	ssize_t ret;
 
 peek_retry:
 	rlen = recv(sock, NULL, 0, MSG_PEEK | MSG_TRUNC);
@@ -52,8 +53,8 @@ peek_retry:
 		return -ENOMEM;
 
 recv_retry:
-	rlen = recv(sock, *nh, rlen, 0);
-	if (rlen < 0) {
+	ret = recv(sock, *nh, rlen, 0);
+	if (ret < 0) {
 		if (errno == EINTR)
 			goto recv_retry;
 
@@ -63,7 +64,7 @@ recv_retry:
 		goto free_resp;
 	}
 
-	*len = rlen;
+	*len = ret;
 
 	return 0;
 