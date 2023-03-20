ap51-flash/ap51-flash

commit 33b44922716398c4c4a00576be84602c05cef20e

ap51-flash: load platform dependent fwupgrade.cfg first
Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>

router_images.c
@@ -55,7 +55,7 @@ extern unsigned long _binary_img_ce_end;
 extern unsigned long _binary_img_ce_size;
 #endif
 
-struct file_info *router_image_get_file(struct list *file_list, char *file_name)
+static struct file_info *_router_image_get_file(struct list *file_list, char *file_name)
 {
 	struct list *list;
 	struct file_info *file_info = NULL, *file_info_tmp;
@@ -63,12 +63,31 @@ struct file_info *router_image_get_file(struct list *file_list, char *file_name)
 	for (list = file_list; list; list = list->next) {
 		file_info_tmp = (struct file_info *)list->data;
 
-		if (strcmp(file_info_tmp->file_name, file_name) != 0)
+		if (strcasecmp(file_info_tmp->file_name, file_name) != 0)
 			continue;
 
 		file_info = file_info_tmp;
 		break;
 	}
+
+	return file_info;
+}
+
+struct file_info *router_image_get_file(struct router_type *router_type, char *file_name)
+{
+	struct file_info *file_info = NULL;
+	char file_name_buff[FILE_NAME_MAX_LENGTH];
+
+	if (strcmp(file_name, "fwupgrade.cfg") == 0) {
+		snprintf(file_name_buff, FILE_NAME_MAX_LENGTH - 1, "%s-%s",
+			 file_name, router_type->desc);
+		file_info = _router_image_get_file(router_type->image->file_list,
+						   file_name_buff);
+	}
+
+	if (!file_info)
+		file_info = _router_image_get_file(router_type->image->file_list,
+						   file_name);
 
 	return file_info;
 }
@@ -79,7 +98,7 @@ static struct file_info *_router_image_add_file(struct router_image *router_imag
 	struct list *list;
 	struct file_info *file_info;
 
-	file_info = router_image_get_file(router_image->file_list, file_name);
+	file_info = _router_image_get_file(router_image->file_list, file_name);
 	if (file_info)
 		goto out;
 

flash.c
@@ -139,7 +139,7 @@ static void node_list_maintain(void)
 			if (ret == 0)
 				break;
 
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: flash complete. Device ready to unplug.\n",
+			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flash complete. Device ready to unplug.\n",
 				node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 				node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 				node->router_type->desc);

proto.c
@@ -191,7 +191,7 @@ static void handle_arp_packet(char *packet_buff, int packet_buff_len, struct nod
 		break;
 	case NODE_STATUS_RESET_SENT:
 	case NODE_STATUS_FINISHED:
-		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: flash complete. Device ready to unplug.\n",
+		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flash complete. Device ready to unplug.\n",
 			node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 			node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 			node->router_type->desc);
@@ -247,9 +247,9 @@ static void handle_udp_packet(char *packet_buff, int packet_buff_len, struct nod
 		switch (node->flash_mode) {
 		case FLASH_MODE_REDBOOT:
 		case FLASH_MODE_TFTP_CLIENT:
-			file_info = router_image_get_file(node->router_type->image->file_list, file_name);
+			file_info = router_image_get_file(node->router_type, file_name);
 			if (!file_info) {
-				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: tftp client asks for '%s' - file not found ...\n",
+				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: tftp client asks for '%s' - file not found ...\n",
 					node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 					node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 					node->router_type->desc, file_name);
@@ -263,7 +263,7 @@ static void handle_udp_packet(char *packet_buff, int packet_buff_len, struct nod
 				node->status = NODE_STATUS_FLASHING;
 			}
 
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: tftp client asks for '%s', serving %s portion of: %s (%i blocks) ...\n",
+			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: tftp client asks for '%s', serving %s portion of: %s (%i blocks) ...\n",
 				node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 				node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 				node->router_type->desc, file_name,file_info->file_name,
@@ -293,7 +293,7 @@ static void handle_udp_packet(char *packet_buff, int packet_buff_len, struct nod
 										FLASH_PAGE_SIZE) * FLASH_PAGE_SIZE;
 				node->image_state.offset = 0;
 
-				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: connection to tftp server established - uploading %i blocks ...\n",
+				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: connection to tftp server established - uploading %i blocks ...\n",
 					node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 					node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 					node->router_type->desc,
@@ -304,12 +304,12 @@ static void handle_udp_packet(char *packet_buff, int packet_buff_len, struct nod
 			node->image_state.block_sent = 0;
 		} else if (block != node->image_state.block_sent) {
 			if (block < node->image_state.block_sent)
-				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: tftp repeat block %d, last received ack: %d\n",
+				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: tftp repeat block %d, last received ack: %d\n",
 					node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 					node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 					node->router_type->desc, block + 1, node->image_state.block_acked);
 			else
-				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: tftp acks unsent block %d (last sent block: %d)\n",
+				fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: tftp acks unsent block %d (last sent block: %d)\n",
 					node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 					node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 					node->router_type->desc, block, node->image_state.block_sent);
@@ -325,7 +325,7 @@ static void handle_udp_packet(char *packet_buff, int packet_buff_len, struct nod
 					switch (node->flash_mode) {
 					case FLASH_MODE_TFTP_SERVER:
 					case FLASH_MODE_TFTP_CLIENT:
-						fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: image successfully transmitted - writing image to flash ...\n",
+						fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: image successfully transmitted - writing image to flash ...\n",
 							node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 							node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 							node->router_type->desc);
@@ -368,19 +368,19 @@ static void handle_udp_packet(char *packet_buff, int packet_buff_len, struct nod
 	/* TFTP error */
 	case 5:
 		if ((block == 2) && (htons(udphdr->len) - sizeof(struct udphdr) > 4))
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: received TFTP error: %s\n",
+			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: received TFTP error: %s\n",
 				node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 				node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 				node->router_type->desc, (packet_buff + sizeof(struct udphdr) + 4));
 		else
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: received TFTP error code: %d\n",
+			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: received TFTP error code: %d\n",
 				node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 				node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 				node->router_type->desc, block);
 
 		break;
 	default:
-		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: unexpected TFTP opcode: %d\n",
+		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: unexpected TFTP opcode: %d\n",
 			node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 			node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 			node->router_type->desc, opcode);

router_images.h
@@ -16,7 +16,7 @@
  * 02110-1301, USA
  */
 
-struct file_info *router_image_get_file(struct list *file_list, char *file_name);
+struct file_info *router_image_get_file(struct router_type *router_type, char *file_name);
 void router_images_init(void);
 void router_images_print_desc(void);
 int router_images_verify_path(char *image_path);

router_redboot.c
@@ -71,7 +71,7 @@ static int redboot_8mb_detect(struct node *node)
 		goto out;
 
 #if defined(DEBUG)
-	fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: flash size of 8 MB was detected ...\n",
+	fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flash size of 8 MB was detected ...\n",
 		node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		node->router_type->desc);
@@ -91,7 +91,7 @@ static int redboot_4mb_detect(struct node (*node)__attribute__((unused)))
 {
 	/* default redboot type */
 #if defined(DEBUG)
-	fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: flash size of 4 MB was detected (default) ...\n",
+	fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flash size of 4 MB was detected (default) ...\n",
 		node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		node->router_type->desc);
@@ -166,7 +166,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 							FLASH_PAGE_SIZE) * FLASH_PAGE_SIZE;
 
 		if (redboot_priv->redboot_type->flash_size < req_flash_size) {
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s: image size '%s' of 0x%08lx exceeds router capacity: 0x%08lx\n",
+			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: image size '%s' of 0x%08lx exceeds router capacity: 0x%08lx\n",
 				node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 				node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 				node->router_type->desc, node->router_type->image->path,
@@ -180,7 +180,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 			((unsigned char *)&node->our_ip_addr)[0], ((unsigned char *)&node->our_ip_addr)[1],
 			((unsigned char *)&node->our_ip_addr)[2], ((unsigned char *)&node->our_ip_addr)[3]);
 
-		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s: setting IP address ...\n",
+		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: setting IP address ...\n",
 		       node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		       node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		       node->router_type->desc);
@@ -205,7 +205,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 			goto redboot_failure;
 		}
 
-		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s: initializing partitions ...\n",
+		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: initializing partitions ...\n",
 		       node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		       node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		       node->router_type->desc);
@@ -222,7 +222,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 			redboot_priv->redboot_type->kernel_load_addr,
 			redboot_priv->redboot_type->kernel_load_addr);
 
-		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s: flashing kernel ...\n",
+		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flashing kernel ...\n",
 		       node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		       node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		       node->router_type->desc);
@@ -241,7 +241,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 		redboot_priv->redboot_state = REDBOOT_STATE_LD_ROOTFS;
 		break;
 	case REDBOOT_STATE_LD_ROOTFS:
-		file_info = router_image_get_file(node->router_type->image->file_list, "kernel");
+		file_info = router_image_get_file(node->router_type, "kernel");
 		if (!file_info)
 			return;
 
@@ -249,7 +249,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 			redboot_priv->redboot_type->flash_addr + file_info->file_fsize,
 			redboot_priv->redboot_type->flash_size - file_info->file_fsize);
 
-		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s: flashing rootfs ...\n",
+		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flashing rootfs ...\n",
 		       node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		       node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		       node->router_type->desc);
@@ -258,7 +258,7 @@ void redboot_main(struct node *node, char *telnet_msg)
 		redboot_priv->redboot_state = REDBOOT_STATE_FL_ROOTFS;
 		break;
 	case REDBOOT_STATE_FL_ROOTFS:
-		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s: setting boot_script_data ...\n",
+		printf("[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: setting boot_script_data ...\n",
 		       node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 		       node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 		       node->router_type->desc);
@@ -355,7 +355,7 @@ out:
 }
 
 const struct router_type redboot = {
-	.desc = "redboot router",
+	.desc = "redboot",
 	.detect_pre = NULL,
 	.detect_main = redboot_detect_main,
 	.detect_post = redboot_detect_post,

router_tftp_client.c
@@ -156,7 +156,7 @@ out:
 }
 
 const struct router_type mr600 = {
-	.desc = "MR600 router",
+	.desc = "MR600",
 	.detect_pre = NULL,
 	.detect_main = mr600_detect_main,
 	.detect_post = tftp_client_detect_post,
@@ -191,7 +191,7 @@ out:
 }
 
 const struct router_type om2p = {
-	.desc = "OM2P router",
+	.desc = "OM2P",
 	.detect_pre = NULL,
 	.detect_main = om2p_detect_main,
 	.detect_post = tftp_client_detect_post,

router_tftp_server.c
@@ -83,7 +83,7 @@ out:
 }
 
 const struct router_type ubnt = {
-	.desc = "ubiquiti router",
+	.desc = "ubiquiti",
 	.detect_pre = ubnt_detect_pre,
 	.detect_main = ubnt_detect_main,
 	.detect_post = ubnt_detect_post,

router_types.c
@@ -113,7 +113,7 @@ int router_types_detect_main(struct node *node, char *packet_buff, int packet_bu
 		ret = 1;
 #endif
 
-		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: type '%s' detected\n",
+		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: type '%s router' detected\n",
 			node->his_mac_addr[0], node->his_mac_addr[1], node->his_mac_addr[2],
 			node->his_mac_addr[3], node->his_mac_addr[4], node->his_mac_addr[5],
 			node->router_type->desc);

commit e91800e62706d3380aa42839ca0e8d432e190021

ap51-flash: Fix maximum size of fwupgrade.cfg file
A filename inside an CE01 image can be up to 32. This is a hard limit from
the index. The FILE_NAME_MAX_LENGTH is therefore set to 33 (32 characters +
\0 terminator) - exactly like the size of file_name_buff used to calculate
the right name for the fwupgrade.cfg in a "COMBINED" CE01 image.

But the snprintf was set to only be allowed to write 32 characters
(including the \0) to this buffer. So not all possible fwupgrade-... names
could be found by this router_image_get_file.

Fixes: 33b449227163 ("ap51-flash: load platform dependent fwupgrade.cfg first")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

router_images.c
@@ -114,7 +114,7 @@ struct file_info *router_image_get_file(struct router_type *router_type,
 	char file_name_buff[FILE_NAME_MAX_LENGTH];
 
 	if (strcmp(file_name, fwupgradecfg) == 0) {
-		snprintf(file_name_buff, FILE_NAME_MAX_LENGTH - 1, "%s-%s",
+		snprintf(file_name_buff, FILE_NAME_MAX_LENGTH, "%s-%s",
 			 file_name,
 			 router_type->image_desc ? router_type->image_desc : router_type->desc);
 		file_info = _router_image_get_file(&router_type->image->file_list,
@@ -122,7 +122,7 @@ struct file_info *router_image_get_file(struct router_type *router_type,
 	}
 
 	if (strcmp(file_name, fwupgradecfgsig) == 0) {
-		snprintf(file_name_buff, FILE_NAME_MAX_LENGTH - 1, "%s-%s.sig",
+		snprintf(file_name_buff, FILE_NAME_MAX_LENGTH, "%s-%s.sig",
 			 fwupgradecfg,
 			 router_type->image_desc ? router_type->image_desc : router_type->desc);
 		file_info = _router_image_get_file(&router_type->image->file_list,