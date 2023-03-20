ap51-flash/ap51-flash

commit 7a4ef891a2294e70ad6d2f1863ef927d1ca5707e

ap51-flash: netconsole - concatenate reset command to reflash
It seems that the reset command sent at the end of the flashing
procedureeis not received by the router netconsole.

To make the procedure more robust, concatenate 'reset'
to the 'fw_upg' command being sent right at the beginning.

This way the uboot shell will simply execute the two
instructions one after the other.

Signed-off-by: Antonio Quartulli <a@unstable.cc>

router_netconsole.c
@@ -64,42 +64,20 @@ void handle_netconsole_packet(const char *packet_buff, int packet_buff_len,
 		break;
 	case NETCONSOLE_STATE_STARTED:
 		/* check if we received the completion message */
-		if (packet_buff_len >= (int)strlen(DONE_STR) &&
-		    strncmp(packet_buff, DONE_STR, strlen(DONE_STR)) == 0) {
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flash complete. Waiting for console.\n",
-				node->his_mac_addr[0], node->his_mac_addr[1],
-				node->his_mac_addr[2], node->his_mac_addr[3],
-				node->his_mac_addr[4], node->his_mac_addr[5],
-				node->router_type->desc);
-
-			priv->state = NETCONSOLE_STATE_DONE;
-			break;
-		}
-		/* fall through */
-	case NETCONSOLE_STATE_DONE:
-		if (packet_buff_len < (int)strlen(PROMPT_STR))
+		if (packet_buff_len < (int)strlen(DONE_STR))
 			return;
 
-		if (strncmp(packet_buff, PROMPT_STR, strlen(PROMPT_STR)) != 0)
+		if (strncmp(packet_buff, DONE_STR, strlen(DONE_STR)) != 0)
 			return;
 
-		if (priv->state == NETCONSOLE_STATE_STARTED) {
-			fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: WARNING reflash - finished but no completion message was received.\n",
-				node->his_mac_addr[0], node->his_mac_addr[1],
-				node->his_mac_addr[2], node->his_mac_addr[3],
-				node->his_mac_addr[4], node->his_mac_addr[5],
-				node->router_type->desc);
-		}
-
-		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: rebooting.\n",
+		fprintf(stderr, "[%02x:%02x:%02x:%02x:%02x:%02x]: %s router: flash complete. Rebooting\n",
 			node->his_mac_addr[0], node->his_mac_addr[1],
 			node->his_mac_addr[2], node->his_mac_addr[3],
 			node->his_mac_addr[4], node->his_mac_addr[5],
 			node->router_type->desc);
 
-		netconsole_reset(node);
+		priv->state = NODE_STATUS_REBOOTED;
 
-		node->status = NODE_STATUS_REBOOTED;
 #if defined(CLEAR_SCREEN)
 		num_nodes_flashed++;
 #endif

proto.c
@@ -190,23 +190,12 @@ int tftp_init_upload(struct node *node)
 				     htons(IPPORT_TFTP), data_len);
 }
 
-int netconsole_reset(struct node *node)
-{
-	int data_len;
-
-	/* finished - send reset command to reboot the router */
-	data_len = sprintf(out_tftp_data, "reset\n");
-
-	return tftp_packet_send_data(node, htons(IPPORT_NETCONSOLE),
-				     htons(IPPORT_NETCONSOLE), data_len);
-}
-
 int netconsole_init_upload(struct node *node)
 {
 	int data_len;
 
-	/* TFTP start command */
-	data_len = sprintf(out_tftp_data, "run fw_upg\n");
+	/* TFTP start command and reset (for subsequential reboot) */
+	data_len = sprintf(out_tftp_data, "run fw_upg; reset\n");
 
 	return tftp_packet_send_data(node, htons(IPPORT_NETCONSOLE),
 				     htons(IPPORT_NETCONSOLE), data_len);

proto.h
@@ -63,7 +63,6 @@ struct image_state {
 int arp_req_send(const uint8_t *src_mac, const uint8_t *dst_mac,
 		 unsigned int src_ip, unsigned int dst_ip);
 int tftp_init_upload(struct node *node);
-int netconsole_reset(struct node *node);
 int netconsole_init_upload(struct node *node);
 void telnet_handle_connection(struct node *node);
 int telnet_send_cmd(struct node *node, const char *cmd);

commit d19b4bc3946955cfe5c7949fa54740be8ed318e3

ap51-flash: netconsole: Split private and node state
The netconsole code has its own internal state which is basically an
embedded state machine in the global node state machine. While this code
was refactored to simplify it, the code to switch the node state
was accidentally dropped and instead it was tried to save the node state in
the netconsole specific state.

This had no effect until now because the NODE_STATUS_REBOOTED (6) is
outside the possible states for the netconsole. And the code handled this
as "nothing to do".

Fixes: 7a4ef891a229 ("ap51-flash: netconsole - concatenate reset command to reflash")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

router_netconsole.c
@@ -125,7 +125,8 @@ void handle_netconsole_packet(const char *packet_buff, int packet_buff_len,
 			node->his_mac_addr[4], node->his_mac_addr[5],
 			node->router_type->desc);
 
-		priv->state = NODE_STATUS_REBOOTED;
+		priv->state = NETCONSOLE_STATE_DONE;
+		node->status = NODE_STATUS_REBOOTED;
 
 #if defined(CLEAR_SCREEN)
 		num_nodes_flashed++;